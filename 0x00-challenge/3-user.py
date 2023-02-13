552ed3d3896a875adcaf4621b60748c5b7c8fa0f
musoye <oyebamijimustapha44@gmail.com>
2023-02-13 12:25:49 +0000

    User password

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -37,10 +37,10 @@ class User():
         - `None` if `pwd` is not a string
         - Hash `pwd` in MD5 before assign to `__password`
         """
-        if pwd is None or type(pwd) is not str:
+        if ((pwd is None) or (type(pwd) is not str)):
             self.__password = None
         else:
-            self._password = hashlib.md5(pwd.encode()).hexdigest().lower()
+            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
     def is_valid_password(self, pwd):
         """
@@ -54,7 +54,7 @@ class User():
             return False
         if self.__password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.password
 
 
 if __name__ == '__main__':
