029589d7c18ee7bd313b75a9342fa6497fa02cb8
musoye <oyebamijimustapha44@gmail.com>
2023-02-20 01:27:26 -0800

    Sort.rb

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -16,11 +16,11 @@ ARGV.each do |arg|
     is_inserted = false
     i = 0
     l = result.size
-    while is_inserted && i < l do
+    while !is_inserted && i < l do
         if result[i] < i_arg
             i += 1
         else
-            result.insert(i - 1, i_arg)
+            result.insert(i, i_arg)
             is_inserted = true
             break
         end
