7a25de4fb9c0abf30b17c2bc2b0b1cc9f459a787
Musoye <oyebamijimustapha44@gmail.com>
2023-04-13 17:43:58 -0700

    Square waoh!

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,7 +2,7 @@
 """Defining the square class module"""
 
 
-class square:
+class square():
     """Square class module"""
     width = 0
     height = 0
@@ -36,3 +36,18 @@ if __name__ == "__main__":
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
+    print('___')
+    b = square(height=3)
+    print(b)
+    print(b.area_of_my_square())
+    print(b.PermiterOfMySquare())
+    print('___')
+    c = square(width=5)
+    print(c)
+    print(c.area_of_my_square())
+    print(c.PermiterOfMySquare())
+    print('___')
+    d = square()
+    print(d)
+    print(d.area_of_my_square())
+    print(d.PermiterOfMySquare())
