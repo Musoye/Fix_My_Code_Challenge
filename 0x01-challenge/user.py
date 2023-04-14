cf33b532e50b50208c722b189e9af9042a2eb80b
Musoye <oyebamijimustapha44@gmail.com>
2023-04-14 10:15:35 -0700

    Step 2: User model

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -1,30 +1,37 @@
 #!/usr/bin/python3
-""" 
-User class
+"""
+User class Module
 """
 
+
 class User():
-    """ Documentation """
+    """User Class Definition"""
 
     def __init__(self):
-        """ Documentation """
-        self.email = None
-
-    @email.setter
-    def email(self, value):
-        """ Documentation """
-        if type(value) is not str:
-            raise TypeError("email must be a string")
-        self.__email = value
+        """Initializatin function"""
+        self.__email = None
 
     @property
     def email(self):
-        """ Documentation """
+        """email getter"""
         return self.__email
-   
-    
+
+    @email.setter
+    def email(self, value):
+        """email setter"""
+        if type(value) is not str or '@' not in value:
+            raise TypeError("email must be a string")
+        self.__email = value
+
+
 if __name__ == "__main__":
 
     u = User()
     u.email = "john@snow.com"
     print(u.email)
+    a = User()
+    try:
+        a.email = 'mustapha'
+        print(a.email)
+    except TypeError:
+        print('TypeError is raise')
